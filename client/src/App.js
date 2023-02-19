f46e15b93240a9b5e89a08ecacf278a129317998
krushna Durole <krushnadurole2000ad@gmail.com>
2023-02-19 18:52:40 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -48,26 +48,106 @@ function App() {
     <>
       <Routes>
 
-        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-
-        {/* <Route path="/account" element={
-          <ProtectedRoute>
-            <Account />
-            </ProtectedRoute>
-          } ></Route> */}
-        <Route path='/account' element={<Account />} />
         <Route path='/categories' element={<Categories />} />
 
-        {/* <Route path="/account/update" element={
-          <ProtectedRoute>
-            <UpdateProfile />
-          </ProtectedRoute>
-        } ></Route>
-        
 
-        <Route path="*" element={<NotFound />}></Route> */}
+
+
+
+        {/* 
+        
+        <Route path='/product/:id' element={<ProductDetails />} />
+        <Route path='/products' element={<Proudcts />} />
+        <Route path='products/:keyword' element={<Products />} />
+
+
+        <Route path='/cart' element={<Cart />} />
+
+
+        <Route path='/order/confirm' element={
+          <OrderConfirm />
+        }>
+        </Route>
+
+        <Route path='/process/payment' element={
+          <Payment />
+        }>
+        </Route>
+
+        <Route path='/order/success' element={<OrderSuccess success={true} />} />
+        <Route path='/order/failed' element={<OrderSuccess />} />
+        <Route path='/order/:id' element={<OrderStatus />} />
+
+        <Route path='/orders'>
+          <MyOrders />
+        </Route>
+
+
+        <Route path='/order_details/:id'>
+          <OrderDetails />
+        </Route>
+
+
+          <Route path='/account' element={<Account />} />
+        <Route path='/account/update' element={
+          <UpdateProfile />
+        }>
+        </Route>
+
+
+        <Route path='/password/update' element={
+          <UpdatePassword />
+        }>
+        </Route>
+
+        <Route path='/password/forgot' element={<ForgotPassword />} />
+        <Route path='/password/rest/:token' element={<ResetPassword />} />
+
+
+        <Route path='/wishlist' element={
+          <Wishlist />
+        }>
+
+
+        </Route>
+        <Route path='/admin/dashboard'>
+          <DashBoard>
+          </DashBoard>
+        </Route>
+
+        <Route path='/admin/orders' element={
+          <DashBoard>
+          </DashBoard>
+        }>
+        </Route>
+
+        <Route path='/admin/order/:id' element={
+          <UpdateOrder/>
+        }>
+        </Route>
+
+        <Route path='/admin/products' element={<ProductTable/>}
+        ></Route>
+
+        <Route path='/admin/new_product' element={<Newproduct/>}></Route>
+        <Route path='/admin/product/:id' element={<UpdateProduct/>}></Route>
+        <Route path='/admin/users' element={<userTable/>}></Route>
+        <Route path='/admin/user/:id' element={<UpdateUser/>}></Route>
+        <Route path='/admin/reviews' element={<ReviewsTable/>}></Route>
+
+
+
+
+ 
+
+        <Route path='*' element={<NotFound/>}></Route> 
+        
+        */}
+
+
       </Routes>
     </>
   );

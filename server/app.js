0e36fcdddd9a29c198de12c92a9edf81f01e205e
krushna Durole <krushnadurole2000ad@gmail.com>
2023-02-28 19:34:29 +0530

    update

--- server/app.js
+++ server/app.js
@@ -22,12 +22,12 @@ app.use(fileUpload());
 const user = require('./routes/userRoute');
 const product = require('./routes/productRoute');
 const order = require('./routes/orderRoute');
-const payment = require('./routes/paymentRoute');
+// const payment = require('./routes/paymentRoute');
 
 app.use('/api/v1', user);
 app.use('/api/v1', product);
 app.use('/api/v1', order);
-app.use('/api/v1', payment);
+// app.use('/api/v1', payment);
 
 // deployment
 __dirname = path.resolve();

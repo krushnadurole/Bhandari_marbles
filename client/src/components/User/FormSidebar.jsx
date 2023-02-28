458b3bea2ee8781bd9cf8312636b5cee10dc65c0
krushna Durole <krushnadurole2000ad@gmail.com>
2023-02-28 19:06:10 +0530

    update

--- client/src/components/User/FormSidebar.jsx
+++ client/src/components/User/FormSidebar.jsx
@@ -1,7 +1,7 @@
 
 const FormSidebar = ({ title, tag }) => {
   return (
-      <div className="bgc primary-violet px-9 py-10 sm:flex flex-col gap-4 w-2/5">
+      <div className="loginSidebar bg-primary-blue px-9 py-10 hidden sm:flex flex-col gap-4 w-2/5">
           <h1 className="font-medium text-white text-3xl">{title}</h1>
           <p className="text-gray-200 text-lg pr-2">{tag}</p>
       </div>
